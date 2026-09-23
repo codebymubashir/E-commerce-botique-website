ecd069f7326e45266074824c7c3e3d5a08cb4287
codebymubashir <unimubashir2@gmail.com>
2026-09-23 09:07:06 -0700

    installing react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white text-2xl font-bold'>
+      welcome to my botique
+      
+    </div>
+  )
+}
+
+export default App
